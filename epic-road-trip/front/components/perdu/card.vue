3a3196974c25b297eb2e6564a2d1971e2cbc4595
Alexandre Douillard <alexandre.douillard@gmail.com>
2022-03-31 23:25:53 +0200

    guide du noob

---
+++ epic-road-trip/front/components/perdu/card.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="perdu d-flex flex-column align-items-center p-5">
+    <span class="exemple__text" @click="$router.push('/')">
+      Vas y clic Mameeene
+    </span>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    yolo: {}
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.perdu {
+  width: 20rem;
+  background: rgba(56, 1, 119, 0.26);
+  border-radius: 0.8rem;
+
+  &__text {
+    color: rgb(189, 189, 189);
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+}
+</style>
--- epic-road-trip/front/pages/index.vue
+++ epic-road-trip/front/pages/index.vue
@@ -1,9 +1,31 @@
 <template>
-  <Tutorial/>
+  <div class="d-flex flex-row justify-content-center pt-4">
+    <HomeExemple :yolo="yolo"/>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+import Vue from "vue";
+
+export default Vue.extend({
+  name: 'IndexPage',
+  data() {
+    return {
+      yolo: {
+        1: {
+          id: 1,
+          text: 'Ceci est un exemple'
+        },
+        2: {
+          id: 2,
+          text: 'c\'est cool',
+        },
+        3: {
+          id: 2,
+          text: 'voila voila'
+        }
+      }
+    }
+  },
+})
 </script>
