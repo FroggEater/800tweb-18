a710776a1eba309735dd405f7fdb11f86e62c486
FroggEater <frogeater@outlook.fr>
2022-04-01 12:12:07 +0200

    ci: update files for automatic versioning

---
+++ back/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd855714143fbffaeb74d44b427b209e2'),
+    },
+  },
+});
---
+++ back/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3d2c32a0-fb35-463a-8476-d493863dfe09'
+};
