a01a07aef05fadc144671c4e05c77bef39ee1854
Alexandra <alexandra.ricardo@epitech.eu>
2022-06-09 12:05:42 +0200

    add data from excel file

--- back/config/server.js
+++ back/config/server.js
@@ -1,9 +1,12 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd855714143fbffaeb74d44b427b209e2'),
+      secret: env("ADMIN_JWT_SECRET", "d855714143fbffaeb74d44b427b209e2"),
     },
+  },
+  cron: {
+    enabled: true,
   },
 });
