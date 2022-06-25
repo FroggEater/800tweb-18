05f609f92e76bd2e7742adc380dc44c82f348ecf
Bastien FROMENT <bastien@FatMac.local>
2022-06-25 16:48:54 +0200

    fix: remove unneeded declarations and add global flex utilities

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,40 +1,18 @@
 <template>
-  <div class="d-flex flex-row justify-content-center pt-4">
-    <HomeExample :yolo="yolo"/>
-    <Input :placeholder="`What's your next stop?`" :small="true"/>
+  <div class="ds-app-container ds-flex-col-center">
+    <!-- <HomeExample :yolo="yolo" /> -->
+    <SharedInput :placeholder="`What's your next stop?`" />
+    <SharedButton icon="user" accent="positive">POSITIVE</SharedButton>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
 import VueFeather from "vue-feather";
-import Input from "../components/shared/Input"
 
 Vue.component("feather", VueFeather);
-Vue.component("Input", Input)
 
 export default Vue.extend({
-  name: 'IndexPage',
-  components: {
-      Input
-    }, 
-  data() {
-    return {
-      yolo: {
-        1: {
-          id: 1,
-          text: 'Ceci est un exemple'
-        },
-        2: {
-          id: 2,
-          text: 'c\'est cool',
-        },
-        3: {
-          id: 3,
-          text: 'voila voila'
-        }
-      }
-    }
-  },
-})
+  name: "PageIndex",
+});
 </script>
