1a248f38fe17700ada83b79d4fe7ad36aa26a02f
Bastien FROMENT <bastien@FatMac.local>
2022-06-25 18:29:16 +0200

    fix: add fonts provider and clean-up global stylesheet

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -1,78 +1,80 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'epic-road-trip',
+    title: "epic-road-trip",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/scss/_colors.scss',
-    '~/assets/scss/helpers.scss',
-    '~/assets/scss/globals.scss'
+    "~/assets/scss/_colors.scss",
+    "~/assets/scss/helpers.scss",
+    "~/assets/scss/globals.scss",
   ],
 
   styleResources: {
-    scss: [
-      '~/assets/scss/_colors.scss'
-    ]
+    scss: ["~/assets/scss/_colors.scss"],
   },
 
-
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/style-resources'
+    "bootstrap-vue/nuxt",
+    "@nuxtjs/style-resources",
+    "@nuxtjs/google-fonts",
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend (config) {
-      config.resolve.alias.vue = 'vue/dist/vue.common'
-    }
+    extend(config) {
+      config.resolve.alias.vue = "vue/dist/vue.common";
+    },
   },
 
   publicRuntimeConfig: {
     strapi: {
-      url: process.env.EPIC_ROAD_TRIP_EXTERNAL_API_URL
+      url: process.env.EPIC_ROAD_TRIP_EXTERNAL_API_URL,
     },
-    EPIC_ROAD_TRIP_EXTERNAL_API_URL: process.env.EPIC_ROAD_TRIP_EXTERNAL_API_URL
+    EPIC_ROAD_TRIP_EXTERNAL_API_URL:
+      process.env.EPIC_ROAD_TRIP_EXTERNAL_API_URL,
   },
 
   privateRuntimeConfig: {
     strapi: {
-      url: process.env.EPIC_ROAD_TRIP_INTERNAL_API_URL
+      url: process.env.EPIC_ROAD_TRIP_INTERNAL_API_URL,
     },
   },
 
   axios: {
-    baseURL: process.env.EPIC_ROAD_TRIP_EXTERNAL_API_URL
+    baseURL: process.env.EPIC_ROAD_TRIP_EXTERNAL_API_URL,
   },
 
   env: {
     VUE_APP_API_URL: process.env.EPIC_ROAD_TRIP_EXTERNAL_API_URL,
   },
-}
+
+  googleFonts: {
+    download: true,
+    families: {
+      Barlow: true,
+    },
+  },
+};
