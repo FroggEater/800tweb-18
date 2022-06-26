f0d55a34489a24f400b8484191d4fa7d16f87507
Bastien FROMENT <bastien@FatMac.local>
2022-06-26 15:06:29 +0200

    fix: correct all SharedButton issues and complete global styles

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="ds-app-container ds-flex-col-center">
     <!-- <HomeExample :yolo="yolo" /> -->
-    <SharedInput :placeholder="`What's your next stop?`" />
-    <SharedButton icon="user" accent="positive">POSITIVE</SharedButton>
+    <!-- <SharedInput :placeholder="`What's your next stop?`" /> -->
+    <SharedButton class="ds-gap-col" icon="user" positive>negative</SharedButton>
+    <SharedButton class="ds-gap-col" positive small>negative</SharedButton>
+    <SharedButton class="ds-gap-col" icon="user" negative>negative</SharedButton>
+    <SharedButton class="ds-gap-col" negative small>negative</SharedButton>
+    <SharedButton class="ds-gap-col" icon="user">neutral</SharedButton>
+    <SharedButton class="ds-gap-col" small>neutral</SharedButton>
+    <SharedButton class="ds-gap-col" icon="user" link>link</SharedButton>
+    <SharedButton class="ds-gap-col" small link>link</SharedButton>
+
+    <SharedButton class="ds-gap-col" icon="settings"/>
+    <SharedButton class="ds-gap-col" icon="settings" small/>
   </div>
 </template>
 
