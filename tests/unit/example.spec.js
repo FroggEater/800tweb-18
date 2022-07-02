d6dc3dc4a7206a44b9b306d4248aa326f902eb00
ErasmeDota <pierre-emmanuel.coq@epitech.eu>
2022-07-02 03:54:33 +0200

    first test should be working
    first basic test
    run npm run test:unit at the base of the project

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,8 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import App from '../../front/components/side/stepper/index.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('index.vue', () => {
+  it('swag', () => {
+    expect(typeof App).toBe('function')
   })
 })
