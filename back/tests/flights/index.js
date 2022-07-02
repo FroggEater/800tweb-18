0f737a237689c1b89e51652d20883e502182166d
alexandraricardo <alexandra.ricardo@epitech.eu>
2022-07-02 11:42:08 +0200

    directions for the test in comments

--- back/tests/flights/index.js
+++ back/tests/flights/index.js
@@ -8,3 +8,7 @@ it("should return flights", async () => {
       expect(data.text).toBe("Hello World!"); // expect the response text
     });
 });
+
+//tester que le token api via la cron se met bien à jour
+// db amadeus token
+//tester que l'api nous renvoie la data qu'on veut
