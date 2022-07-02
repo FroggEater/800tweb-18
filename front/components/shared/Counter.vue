a48e77f84bc48746a44b5ade566955ac29ceeb1d
ErasmeDota <pierre-emmanuel.coq@epitech.eu>
2022-07-02 04:57:01 +0200

    Adding first test to press a button
    Created fake Counter.vue file to do some basics test

---
+++ front/components/shared/Counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>counter: {{ counter }}</div>
+    <button @click="counter += 1"></button>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    counter: 0
+  })
+}
+</script>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,8 +1,30 @@
 import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+// lazy && needs to be fixed using absolute path
 import App from '../../front/components/side/stepper/index.vue'
+import Counter from '../../front/components/shared/Counter.vue'
+import Vue from 'vue'
 
 describe('index.vue', () => {
   it('swag', () => {
     expect(typeof App).toBe('function')
+  })
+})
+
+describe('Counter', () => {
+  test('Setup', () => {
+    expect(true).toBe(true)
+  })
+})
+
+describe('Counter Mount', () => {
+  test('Increment Counter', async () => {
+    const wrapper = mount(Counter);
+    expect(wrapper.text()).toContain('counter: 0')
+    wrapper.find('button').trigger('click')
+    await Vue.nextTick()
+    console.log(wrapper.find('button').trigger('click').then)
+    expect(wrapper.text()).toContain('counter: 1')
+    console.log(wrapper.find('button'))
   })
 })
