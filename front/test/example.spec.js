6c3944388fbf062dde99c5f74d67bd0c543aa714
ErasmeDota <pierre-emmanuel.coq@epitech.eu>
2022-07-03 18:20:32 +0200

    Fixing bug from updating with master

---
+++ front/test/example.spec.js
@@ -0,0 +1,28 @@
+import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
+// lazy && needs to be fixed using absolute path
+import Counter from '../components/shared/Counter.vue'
+import Button from '../components/shared/Button.vue'
+import Vue from 'vue'
+
+describe('Button Test', () => {
+  test('basic button', async () => {
+    const wrapper = mount(Button);
+    expect(wrapper.exists()).toBe(true)
+  })
+})
+
+
+describe('Counter Mount', () => {
+  test('Counter exists?', async () => {
+    const wrapper = mount(Counter);
+    expect(wrapper.exists()).toBe(true)
+  })
+  test('Increment Counter', async () => {
+    const wrapper = mount(Counter);
+    expect(wrapper.text()).toContain('counter: 0')
+    wrapper.find('button').trigger('click')
+    await Vue.nextTick()
+    expect(wrapper.text()).toContain('counter: 1')
+  })
+})
